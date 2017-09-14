b14bdc4e8d34a628081758703c941a8f1cfa4063
AIChallenger <zhenghe@interns.chuangxin.com>
2017-09-14 16:15:46 +0800

    add caption_baseline

---
+++ Baselines/caption_baseline/build_tfrecord.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+export CUDA_VISIBLE_DEVICES="1"
+python build_tfrecord.py --image_id=/home/store-1-img/wenjia/challenger_dataset/caption/ai_challenger_caption_train_20170902/caption_train_images_20170902\
+                        --captions_file=/home/store-1-img/wenjia/challenger_dataset/caption/ai_challenger_caption_train_20170902/caption_train_annotations_20170902.json\
+                        --output_dir=/home/store-1-img/zhenghe/ai_challenger_caption_train_output\
+                        --train_shards=280\
+                        --num_threads=56
---
+++ Baselines/caption_baseline/im2txt/README.md
@@ -0,0 +1,12 @@
+# Show and Tell: A Neural Image Caption Generator
+
+A TensorFlow implementation of the image-to-text model described in the paper:
+
+"Show and Tell: Lessons learned from the 2015 MSCOCO Image Captioning
+Challenge."
+
+Oriol Vinyals, Alexander Toshev, Samy Bengio, Dumitru Erhan.
+
+*IEEE transactions on pattern analysis and machine intelligence (2016).*
+
+Full text available at: http://arxiv.org/abs/1609.06647
